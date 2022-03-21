34873b428d34b762cfb24001cbe9ec06a1cd5de1
victor li <victor.li@esme.fr>
2022-03-21 12:38:23 +0100

    Code J1

--- SensCritique.py
+++ SensCritique.py
@@ -4,7 +4,8 @@ import selenium
 import time
 
 driver = webdriver.Firefox()
-Liste_Films = []
+Liste_Films = [{ 'Nom_film' : 'film', 'Note': 8.6}]
+Liste_note = []
 driver.get("https://www.senscritique.com/")
 time.sleep(2)
 #driver.find_element(by= By.CLASS_NAME, value="fc-button-label").click()
@@ -14,9 +15,5 @@ driver.find_element_by_class_name('header-navigation-main-item').click()
 time.sleep(2)
 driver.find_element_by_link_text('TOPS').click()
 time.sleep(2)
-if  driver.find_element_by_class_name('epca-cover type-1')== True :
-    driver.find_element_by_class_name('epca-cover type-1').click()
-else:
-    Ignore
-time.sleep(2)
 driver.find_element_by_class_name('epca-title').click()
+
