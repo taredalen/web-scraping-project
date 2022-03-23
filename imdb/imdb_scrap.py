a15fd3c7f9b24e51d8b1bb88cda798b3fc22db13
taredallen <51906844+taredallen@users.noreply.github.com>
2022-03-24 00:57:24 +0100

    24.03 some changes + .csv file

--- imdb/imdb_scrap.py
+++ imdb/imdb_scrap.py
@@ -1,28 +1,48 @@
+import csv
 import time, requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
 request = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
-soup = BeautifulSoup(request.text, 'lxml')
 
-fieldnames = ['name', 'year', 'genre', 'rating']
+fieldnames = ['link', 'name', 'year', 'genre', 'rating', 'original name']
 rows = []
 
 liste_des_filmes = []
 
-for film in range(0, 1):
+for film in range(0, 2):
+    time.sleep(1)
     dictionnaire = {}
 
-    filmes = soup.find("tbody", {"class": "lister-list"}).find_all('tr')[film]
+    soup = BeautifulSoup(request.text, 'lxml')
+    filmes = soup.find('tbody', {'class': 'lister-list'}).find_all('tr')[film]
 
-    name = filmes.find_all('td')[1].find('a').text
-    year = filmes.find_all('td')[1].find('span').text.replace('(', '')
-    score = filmes.find_all('td')[2].find('strong').text
-    genre = filmes.find_all('td')[2].find('strong').text
-    rating = filmes.find_all('td')[2].find('strong').text
+    link = 'https://www.imdb.com/' + filmes.find_all('td')[1].find('a').get('href')
 
+    soup = BeautifulSoup(requests.get(link).text, 'lxml')
+
+    year = soup.find('a', {'class': 'ipc-link ipc-link--baseAlt ipc-link--inherit-color sc-52284603-1 ifnKcw'}).text
+    name = soup.find('h1', {'class': 'sc-b73cd867-0 eKrKux'}).text
+    rating = soup.find('span', {'class': 'sc-7ab21ed2-1 jGRxWM'}).text
+    original_name = soup.find('div', {'class': 'sc-dae4a1bc-0 gwBsXc'}).text[16:]
+
+    liste_genres = []
+    genres = soup.find('div', {'class': 'ipc-chip-list sc-14389611-4 ctpXmw'}).find_all('span', {'class': 'ipc-chip__text'})
+    for genre in genres:
+        liste_genres.append(genre.text)
+
+    dictionnaire['link'] = link
     dictionnaire['name'] = name
     dictionnaire['year'] = year.replace(')', '')
+    dictionnaire['genre'] = ', '.join(liste_genres)
+    dictionnaire['rating'] = rating
+    dictionnaire['original name'] = original_name
+
     rows.append(dictionnaire)
 
+print(rows)
 
+
+with open('imdb.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(rows)
