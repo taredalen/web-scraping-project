7ab4383c064fdecd316fbcc9cb272bb060ec71c6
victor li <victor.li@esme.fr>
2022-03-23 22:55:50 +0100

    fetch all movies titles

--- SensCritique.py
+++ SensCritique.py
@@ -1,4 +1,4 @@
-from csv import list_dialects
+
 from tokenize import Ignore
 from selenium import webdriver
 import pandas as pd
@@ -9,6 +9,7 @@ driver = webdriver.Firefox()
 
 Liste_note = []
 liste_film =[]
+Liste_film_traite = []
 driver.get("https://www.senscritique.com/")
 time.sleep(5)
 #driver.find_element(by= By.CLASS_NAME, value="fc-button-label").click()
@@ -21,11 +22,10 @@ time.sleep(5)
 driver.find_element_by_class_name('epca-title').click()
 time.sleep(5)
 
-#if driver.find_element_by_class_name('sas-btn-skip sas-enable')==True:
-#    driver.find_element_by_class_name('sas-btn-skip sas-enable').click()
-
 time.sleep(2)
-driver.find_elements_by_class_name('elco-anchors')
-print(driver.find_elements_by_class_name('d-heading2 elco-title'))
 
-#Films = pd.DataFrame({"Nom_Film":liste_film,"Note":Liste_note})
+liste_film.append(driver.find_elements_by_class_name('elco-title'))
+for element in range(110):
+    Liste_film_traite.append(liste_film[0][element].text)
+
+print(Liste_film_traite)
