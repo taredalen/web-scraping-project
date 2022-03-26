62deabb1b7fa45d32716280f580ac523cc6d6d5a
victor li <victor.li@esme.fr>
2022-03-26 18:03:41 +0100

    Srap Sens critique Fonctionnelle dans CSV, => To JSON in progress

--- SensCritique/SCget_review.py
+++ SensCritique/SCget_review.py
@@ -1,22 +1,15 @@
-from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 import pandas as pd
 
 
 
-Liste_commentaire = []
-Liste_commentaire_titre = []
-
-def getCommentaire(driver):
-    Liste_commentaire_titre.append(driver.find_elements(By.CLASS_NAME,'ere-review-heading').text)
-    Liste_commentaire.append(driver.find_elements(By.CLASS_NAME,'ere-review-excerpt').text)
+def getCommentaire(Liste_titre,liste_comm,driver):
+    Liste_titre.append(driver.find_elements(By.CLASS_NAME,'ere-review-heading'))
+    liste_comm.append(driver.find_elements(By.CLASS_NAME,'ere-review-excerpt'))
 
 
-def merge_comms(liste_comm,liste_titre):
+def merge_comms(liste_titre,liste_comm):
     Frame_commentaire = pd.DataFrame({"titre":liste_titre,"commentaire":liste_comm})
     Frame_commentaire.to_csv('SensCritique.csv',sep = ";")
     return Frame_commentaire
-
-def recherche(titre,driver):
-    recherche = driver.find_element(By.CLASS_NAME,'_25jdusMm9PFEdy9TPVD0IK')
-    recherche.send_keys(titre)
--- SensCritique/SensCritique.py
+++ SensCritique/SensCritique.py
@@ -17,11 +17,14 @@ from SCget_review import recherche
 driver = webdriver.Firefox()
 
 Liste_note = []
-liste_film =[]
+liste_film = []
+liste_film_annee = []
 Liste_film_traite = []
 Liste_complete = {}
 dictionnaire = {}
 Liste_film_to_search = []
+Liste_commentaire = []
+Liste_commentaire_titre = []
 
 driver.get("https://www.senscritique.com/")
 time.sleep(5)
@@ -41,22 +44,37 @@ for element in range(100):
     Liste_film_traite.append(liste_film[0][element].text)
 
 
+for film in range (len(Liste_film_traite)):
+    Liste_film_traite[film] = Liste_film_traite[film].replace("("," ")
+    Liste_film_traite[film] = Liste_film_traite[film].replace(")","")
+    Liste_film_traite[film] = Liste_film_traite[film].split("  ")
 
+
+
+#Liste_complete = {"titre":Liste_film_traite}
 
 Data_imdb = pd.read_json('./imdb/data.json')
 
 
-for titre in Liste_film_traite:
-    recherche(titre,driver)
-    
+for titre in range(len(Liste_film_traite)):
+    recherche = driver.find_element(By.CLASS_NAME,'_25jdusMm9PFEdy9TPVD0IK')
+    recherche.send_keys(Liste_film_traite[titre][0])
+    time.sleep(5)
+    Explorer = driver.find_element(By.CLASS_NAME,'_3mHi2AhyGxzjT4yEFxyS1g')
+    time.sleep(5)
+    Explorer.click()
+    time.sleep(5)
+    getCommentaire(Liste_commentaire_titre,Liste_commentaire,driver)
+    time.sleep(5)
+    merge_comms(Liste_commentaire_titre,Liste_commentaire)
 
 
 
 #dictionnaire['commentaire'] = Liste_commentaire
 #dictionnaire['titre_commentaire'] = Liste_commentaire_titre
-#Liste_complete[Liste_film_traite] = dictionnaire
+#Liste_complete.append(dictionnaire)
 
 #with open('data_Senscritique.json','w') as outfile:
-#    json.dump(dictionnaire, outfile, indent = 1)
+#    json.dump(Liste_complete, outfile, indent = 1)
 
 driver.close()
