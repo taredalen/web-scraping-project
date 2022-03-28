f4c9b8f69a2fa632da64cc8aacc1fbdb8551100b
victor li <victor.li@esme.fr>
2022-03-28 14:20:37 +0200

    WIP, need to add other values

--- SensCritique/SCget_review.py
+++ SensCritique/SCget_review.py
@@ -5,15 +5,17 @@ import pandas as pd
 
 
 def getCommentaire(Liste_titre,liste_comm,driver):
-    Liste_titre.append(driver.find_elements(By.CLASS_NAME,'ere-review-heading'))
-    liste_comm.append(driver.find_elements(By.CLASS_NAME,'ere-review-excerpt'))
+    titre2 = driver.find_elements(By.CLASS_NAME,'ere-review-heading')
+    commentaire = driver.find_elements(By.CLASS_NAME,'ere-review-excerpt')
+    for i in range(len(titre2)):
+        titre2[i] = titre2[i].text
+    Liste_titre.append(titre2)
+    for j in range(len(commentaire)):
+        commentaire[j] = commentaire[j].text
+    liste_comm.append(commentaire)
 
 
 def merge_comms(liste_titre,liste_comm):
     Frame_commentaire = pd.DataFrame({"titre_comm":liste_titre,"commentaire":liste_comm})
     Frame_commentaire.to_csv('SensCritique.csv',sep = ";")
     return Frame_commentaire
-
-def traitement_valeurs(Liste):
-    for values in range (len(Liste[0])):
-        Liste[0][values] = Liste[0][values].text
