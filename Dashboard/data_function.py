5f0984970089efd56a9be8e41c00e6549c650efc
taredallen <51906844+taredallen@users.noreply.github.com>
2022-04-11 18:28:59 +0200

    app update direction

--- Dashboard/data_function.py
+++ Dashboard/data_function.py
@@ -3,36 +3,64 @@ import pandas as pd
 from pandas import Series
 
 def get_json_data():
-    with open('Data/final_data2.json', 'r') as f:
+    with open('../Data/final_data2.json', 'r') as f:
         return json.loads(f.read())
 
-def normalize_data(data):
+def normalize_data():
+    data = get_json_data()
     df = pd.json_normalize(data, meta='title', record_path=['results'])
 
     df['year'] = df['year'].astype(int)
     df['rating'] = pd.to_numeric(df['rating'])
     df['metascore'] = pd.to_numeric(df['metascore'])/10
-    df["rating sc"] = pd.to_numeric(df["rating sc"])
-    return df[['title', 'year', 'rating', 'metascore', 'genre', 'rating sc']]
+    df['rating sc'] = pd.to_numeric(df['rating sc'])
+    df['decade'] = (10 * (df['year'] // 10)).astype(str) + 's'
+    return df[['title', 'year', 'rating', 'metascore', 'genre', 'rating sc', 'decade']]
 
-def get_genre_by_decades(df_c):
-    s = df_c['genre'].str.split(' ').apply(Series, 1).stack()
+def get_genre_by_decades():
+
+    df = normalize_data()
+
+    s = df['genre'].str.split(' ').apply(Series, 1).stack()
     s.index = s.index.droplevel(-1)
     s.name = 'genre'
-    del df_c['genre']
-    df2 = df_c.join(s)
+    del df['genre']
+    df2 = df.join(s)
 
     df2 = df2.groupby(['year', 'genre'], sort=True)['year'].count()
-    df_year = df2.to_frame(name = 'genre count').reset_index()
+    df_year = df2.to_frame(name='genre count').reset_index()
 
     df_year['decade'] = (10 * (df_year['year'] // 10)).astype(str) + 's'
 
     df_dec = df_year.groupby(['decade', 'genre'], sort=True)['year'].count()
-    df_dec = df_dec.to_frame(name = 'genre count').reset_index()
+    df_dec = df_dec.to_frame(name='genre count').reset_index()
+
     return df_dec
 
-def get_movies_by_decade(df):
-    df['decade'] = (10 * (df['year'] // 10)).astype(str) + 's'
-    df = df.groupby(['decade'],sort=True)['decade'].count()
+def get_movies_by_decade(decade):
+
+    df = normalize_data()
+
+    index_decades = df[df['decade'] != decade].index
+
+    df.drop(index_decades, inplace=True)
+
+    s = df['genre'].str.split(' ').apply(Series, 1).stack()
+    s.index = s.index.droplevel(-1)
+    s.name = 'genre'
+    del df['genre']
+    df2 = df.join(s)
+    return df2
+
+def get_movie_score(title):
+    df = normalize_data()
+
+    index_title = df[df['title'] != title].index
+    df.drop(index_title, inplace=True)
+
     return df
-    
+
+def get_movies_count_by_decade():
+    df = normalize_data()
+    df = df.groupby(['decade'], sort=True)['decade'].count()
+    return df
--- Dashboard/graph_function.py
+++ Dashboard/graph_function.py
@@ -1,53 +1,18 @@
-from dash import Dash, html, dcc
-import plotly.express as px
+import random
 import plotly
 import plotly.graph_objs as go
-import random
-import sys, os
-sys.path.insert(0, str(os.getcwd()) + '/Dashboard')
-from data_function import get_movies_by_decade
-
-def get_menu_graph(dataframe, dataframe_decade):
-    
-    return html.Div(children=[
-        dcc.Graph(id="rating-graph", 
-        figure=px.bar(dataframe, x="title", y=['rating', 'metascore', 'rating sc'], barmode='group', title="User rating and metascore"), 
-        style={'width': '210vh', 'height': '100vh'}),
-        dcc.Graph(id="movie-count-graph", 
-        figure=px.bar(get_movies_by_decade(dataframe), x="decade", labels={
-            'decade': 'movies',
-            'index': 'decade'
-        })),
-        dcc.Graph(id="genre-graph", 
-        figure=px.bar(dataframe_decade, x="decade", y="genre count", color='genre', barmode="group", title="Popular genre per decade"))
-    ])
+from dash import html, dcc
 
 def get_page_film(list_film, film_name):
     film = next((dictionary for dictionary in list_film if dictionary["title"] == film_name), None)
     if film is not None:
-        info = get_film_info(film)
         film_result = film['results'][0]
-        adj_user_wordcloud = get_wordcloud(film_result["users reviews"], "Users most used adjectives")
-        adj_pro_wordcloud = get_wordcloud(film_result["critics reviews"], "Critics most used adjectives")
-        adj_sc_workcloud = get_wordcloud(film_result["reviews sc"], "Senscritique users most used adjectives")
-        return [info, get_ratings(film),  adj_user_wordcloud, adj_pro_wordcloud, adj_sc_workcloud]
+        adj_user_wordcloud = get_wordcloud(film_result["users reviews"], "USERS MOST USED ADJECTIVES")
+        adj_pro_wordcloud = get_wordcloud(film_result["critics reviews"], "CRITICS MOST USED ADJECTIVE")
+        adj_sc_workcloud = get_wordcloud(film_result["reviews sc"], "SENSCRITIQUE USERS MOST USED ADJECTIVES")
+        return [html.P(" "), adj_user_wordcloud, adj_pro_wordcloud, adj_sc_workcloud]
 
-def get_ratings(film):
-    fig = go.Figure(data=[
-        go.Bar(name="user rating", y=[float(film["results"][0]["rating"])]),
-        go.Bar(name="metascore", y=[float(film["results"][0]["metascore"])/10]),
-        go.Bar(name="Senscritique rating", y=[float(film["results"][0]["rating sc"])])
-    ])
-    return dcc.Graph(id='rating-movie-graph',
-    figure=fig, style={"width": "150vh"})
 
-def get_film_info(film):
-    info = "Title: " + film["title"] + " Year: " + film["results"][0]["year"] + " Genre: " + film["results"][0]["genre"]
-    return html.Div(info)
-
-"""
-renvoie les 30 adjectifs les plus utilisé sous formes de wordcloud
-"""
 def get_wordcloud(reviews, title):
     adj = []
     for review in reviews:
@@ -58,26 +23,41 @@ def get_wordcloud(reviews, title):
     adj = wordcount(adj)
     adj = sorted(adj, key=lambda x: x[1], reverse=True)
     words = [word for word, _ in adj]
-    weights = [count*8 for _, count in adj]
-    #weights = [random.randint(15, 35) for i in range(30)]
-    if len(words) > 30: 
+    weights = [count * 8 for _, count in adj]
+    # weights = [random.randint(15, 35) for i in range(30)]
+    if len(words) > 30:
         words = words[:30]
         weights = weights[:30]
     colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in range(len(words))]
     data = go.Scatter(x=[random.random() for i in range(len(words))],
-                 y=[ random.random() for i in range(len(words))],
-                 mode='text',
-                 text=words,
-                 marker={'opacity': 0.3}, 
-                 textfont={'size': weights,
-                           'color': colors})
-    layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
-                    'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
-    
-    fig = go.Figure(data=[data], layout=layout)
-    fig.update_layout(title_text=title)
+                      y=[random.random() for i in range(len(words))],
+                      mode='text',
+                      text=words,
+                      marker={'opacity': 0.3},
+                      textfont={'size': weights,
+                                'color': colors}
+                      )
 
-    return dcc.Graph(figure=fig, style={'height':'100vh'})
+    layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
+                        'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
+
+    fig = go.Figure(data=[data], layout=layout)
+    fig.update_layout(
+        #title_text=title,
+        title={
+            'text': title,
+            'y': 0.95,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        #legend=dict(title=None, orientation='v',  yanchor='bottom', x=0.5, xanchor='center'),
+        plot_bgcolor='#323130',
+        paper_bgcolor='#323130',
+        font=dict(color='white'),
+        xaxis_title=None,
+        yaxis_title=None
+    )
+    return dcc.Graph(figure=fig, style={'height': '100vh'})
 
 
 def wordcount(list_word):
@@ -89,7 +69,7 @@ def wordcount(list_word):
     for word, count in list_word:
         if current_word == word:
             current_count += count
-        else :
+        else:
             if current_word is not None:
                 wordcount_list.append((current_word, current_count))
             current_word = word
